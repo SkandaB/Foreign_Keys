d564716b536c3bf854ebc8034e102a4a446e0206
Tuğçe Akın <tugce.eylul.akin@hotmail.com>
2016-11-07 18:34:16 -0800

    Update generate_example.py

--- HW7/generate_example.py
+++ HW7/generate_example.py
@@ -13,6 +13,22 @@ db = MySQLdb.connect(host="127.0.0.1",    # your host, usually localhost
 
 cur = db.cursor()
 
+#User table
+for i in range(6, 101):
+    try:
+        full_name = fake.name()
+        street = fake.street_address()
+        city = fake.city()
+        unit_number = fake.random_digit_not_null()
+        zip_code = fake.zipcode()
+        #SQL query to INSERT a record into the table FACTRESTTBL.
+        cur.execute('''INSERT into user (id, full_name, Street, City, unit_number, zip_code)
+                      values (%s, %s, %s, %s, %s, %s)''',
+                      (i, full_name, street, city, unit_number, zip_code))
+        db.commit()
+    except:
+       db.rollback()
+
 #Vote table
 for i in range(10, 101):
     try:
