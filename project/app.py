fd857bf6af628c63adf104014aa776a365202441
tugceakin <tugce.eylul.akin@hotmail.com>
2016-11-20 17:57:24 -0800

    Remove console log

--- project/app.py
+++ project/app.py
@@ -65,21 +65,20 @@ def show_question(question_id):
     return render_template('question.html', answers = answers, question = question, tags = tags)
 
 
-# @app.route('/add', methods=['POST'])
-# def add_entry():
-#     if not session.get('logged_in'):
-#         abort(401)
-#     db = get_db()
-#     db.execute('insert into entries (title, text) values (?, ?)',
-#                [request.form['title'], request.form['text']])
-#     db.commit()
-#     flash('New entry was successfully posted')
-#     return redirect(url_for('show_entries'))
+@app.route('/add_question', methods=['POST'])
+def add_entry():
+    if not session.get('logged_in'):
+        abort(401)
+    db = get_db()
+    db.execute('insert into entries (title, text) values (?, ?)',
+               [request.form['title'], request.form['text']])
+    db.commit()
+    flash('New entry was successfully posted')
+    return redirect(url_for('show_question_list'))
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    print 'in log'
     db = get_db()
     cur = db.cursor()
     error = None
